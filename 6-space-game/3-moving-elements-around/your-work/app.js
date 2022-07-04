2be695b211c0f163e0fb3ebe952d22c253432527
saltimbanki <jorgebabarro@hotmail.com>
2022-07-04 11:51:09 +0200

    creacion de clases y pintado y movimiento de los enemigos

--- 6-space-game/3-moving-elements-around/your-work/app.js
+++ 6-space-game/3-moving-elements-around/your-work/app.js
@@ -1,3 +1,81 @@
+class GameObject {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.dead = false;
+    this.type = "";
+    this.width = 0;
+    this.height = 0;
+    this.img = undefined;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+}
+
+class Hero extends GameObject {
+  constructor(x, y) {
+    super(x,y);
+    this.x = x;
+    this.y = y;
+    this.type = "hero";
+    this
+    this.speed = 1;
+  }
+}
+
+class Enemy extends GameObject {
+  constructor(x, y) {
+    super(x, y);
+    (this.width = 98), (this.height = 50);
+    this.type = "Enemy";
+    let id = setInterval(() => {
+      if (this.y < canvas.height - this.height) {
+        this.y += 5;
+      } else {
+        console.log("Stopped at ", this.y);
+        clearInterval(id);
+      }
+    }, 300);
+  }
+}
+
+class EventEmitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(message, listener) {
+    if (!this.listeners[message]) {
+      this.listeners[message] = [];
+    }
+    this.listeners[message].push(listener);
+  }
+
+  emit(message, payload = null) {
+    if (this.listeners[message]) {
+      this.listeners[message].forEach((l) => l(message, payload));
+    }
+  }
+}
+
+const Messages = {
+  KEY_EVENT_UP: "KEY_EVENT_UP",
+  KEY_EVENT_DOWN: "KEY_EVENT_DOWN",
+  KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
+  KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
+};
+
+let heroImg,
+  enemyImg,
+  laserImg,
+  canvas,
+  ctx,
+  gameObjects = [],
+  hero,
+  eventEmitter = new EventEmitter();
+
 function loadTexture(path) {
   return new Promise((resolve) => {
     const img = new Image();
@@ -8,7 +86,7 @@ function loadTexture(path) {
   });
 }
 
-function createEnemies(ctx, canvas, enemyImg) {
+function createEnemies() {
   const MONSTER_TOTAL = 5;
   const MONSTER_WIDTH = MONSTER_TOTAL * 98;
   const START_X = (canvas.width - MONSTER_WIDTH) / 2;
@@ -16,25 +94,40 @@ function createEnemies(ctx, canvas, enemyImg) {
 
   for (let x = START_X; x < STOP_X; x += 98) {
     for (let y = 0; y < 50 * 5; y += 50) {
-      ctx.drawImage(enemyImg, x, y);
+      const enemy = new Enemy(x, y);
+      enemy.img = enemyImg;
+      gameObjects.push(enemy);
     }
   }
+}
+
+function createHero() {
+  hero = new Hero(
+    canvas.width / 2 - 45,
+    canvas.height - canvas.height / 4
+  );
+  hero.img = heroImg;
+  gameObjects.push(hero);
+}
+
+function drawGameObjects(ctx){
+  gameObjects.forEach(go => go.draw(ctx));
 }
 
 window.onload = async () => {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  const heroImg = await loadTexture("assets/player.png");
-  const enemyImg = await loadTexture("assets/enemyShip.png");
+  heroImg = await loadTexture("assets/player.png");
+  enemyImg = await loadTexture("assets/enemyShip.png");
+  laserImg = await loadTexture("assets/laserRed.png");
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(
-    heroImg,
-    canvas.width / 2 - 45,
-    canvas.height - canvas.height / 4
-  );
-  createEnemies(ctx, canvas, enemyImg);
+  initGame();
+  let gameLoopId = setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawGameObjects(ctx);
+  }, 100);
 };
 
 let onKeyDown = function (e) {
@@ -53,4 +146,38 @@ let onKeyDown = function (e) {
   }
 };
 
-window.addEventListener('keydown', onKeyDown);
+window.addEventListener("keydown", onKeyDown);
+
+window.addEventListener("keyup", (evt) => {
+  if (evt.key === "ArrowUp") {
+    eventEmitter.emit(Messages.KEY_EVENT_UP);
+  } else if (evt.key === "ArrowDown") {
+    eventEmitter.emit(Messages.KEY_EVENT_DOWN);
+  } else if (evt.key === "ArrowLeft") {
+    eventEmitter.emit(Messages.KEY_EVENT_LEFT);
+  } else if (evt.key === "ArrowRight") {
+    eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
+  }
+});
+
+function initGame() {
+  gameObjects = [];
+  createEnemies();
+  createHero();
+
+  eventEmitter.on(Messages.KEY_EVENT_UP, () => {
+    hero.y -= 5;
+  });
+
+  eventEmitter.on(Messages.KEY_EVENT_DOWN, () => {
+    hero.y += 5;
+  });
+
+  eventEmitter.on(Messages.KEY_EVENT_LEFT, () => {
+    hero.x -= 5;
+  });
+
+  eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
+    hero.x += 5;
+  });
+}
