c9b8bfaa070ef017d46388cfabf10ffd3c77e636
saltimbanki <jorgebabarro@hotmail.com>
2022-07-04 05:53:00 +0200

    aprendizaje de chrome runtime cambiando el color del icono

--- 5-browser-extension/start/dist/background.js
+++ 5-browser-extension/start/dist/background.js
@@ -1,4 +1,20 @@
 //aggiungere qui il listener
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+    if(msg.action === 'updateIcon'){
+        chrome.browserAction.setIcon({imageData: drawIcon(msg.value)});
+    }
+});
 
 //presa in prestito dall'estensione energy lollipop
 //disegnare qui l'icona
+function drawIcon(value){
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.fillStyle = value.color;
+    context.arc(100,100, 50, 0 , 2 * Math.PI);
+    context.fill();
+
+    return context.getImageData(50,50,100,100);
+}
