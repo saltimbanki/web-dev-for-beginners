2132e9360f4de35a302c8ba2f9caa1290cea46be
saltimbanki <jorgebabarro@hotmail.com>
2022-07-05 12:30:19 +0200

    añade movimiento al maincharacter

--- 6-space-game/4-collision-detection/collision_assignment/index.js
+++ 6-space-game/4-collision-detection/collision_assignment/index.js
@@ -1,3 +1,22 @@
+class EventEmitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(message, listener) {
+    if (!this.listeners[message]) {
+      this.listeners[message] = [];
+    }
+    this.listeners[message].push(listener);
+  }
+
+  emit(message, payload = null) {
+    if (this.listeners[message]) {
+      this.listeners[message].forEach((l) => l(message, payload));
+    }
+  }
+}
+
 class GameObject {
   constructor(x, y) {
     this.x = x;
@@ -33,6 +52,7 @@ class MainCharacter extends GameObject {
     this.height = 100;
     this.type = "MainCharacter";
     this.color = "green";
+    this.speed = { x: 0, y: 0 };
   }
 }
 
@@ -54,6 +74,14 @@ class Enemy extends GameObject {
     }, 100);
   }
 }
+//declaracion de mensages
+const Messages = {
+  KEY_EVENT_UP: "KEY_EVENT_UP",
+  KEY_EVENT_DOWN: "KEY_EVENT_DOWN",
+  KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
+  KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
+  COLLISION_ENEMY_MAIN: "COLLISION_ENEMY_MAIN",
+};
 
 //declaracion de variables y constantes
 let canvas,
@@ -64,31 +92,73 @@ let canvas,
   mainCharacterImpactColor,
   enemyColor,
   spawnPoints = [],
-  timeSpawn;
-  //   eventEmitter = new EventEmitter();
+  timeSpawn,
+  eventEmitter = new EventEmitter();
+
+//eventos
+let onKeyDown = function (e) {
+  console.log(e);
+  switch (e.keyCode) {
+    case 37:
+    case 39:
+    case 38:
+    case 40:
+    case 32:
+      e.preventDefault();
+      break;
+    default:
+      break;
+  }
+};
+
+window.addEventListener("keydown", onKeyDown);
+
+window.addEventListener("keydown", (evt) => {
+  if (evt.key === "ArrowUp") {
+    eventEmitter.emit(Messages.KEY_EVENT_UP);
+  } else if (evt.key === "ArrowDown") {
+    eventEmitter.emit(Messages.KEY_EVENT_DOWN);
+  } else if (evt.key === "ArrowLeft") {
+    eventEmitter.emit(Messages.KEY_EVENT_LEFT);
+  } else if (evt.key === "ArrowRight") {
+    eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
+  }
+});
 
 function initGame() {
   gameObjects = [];
   spawnEnemies();
   createMainCharacter();
-  drawGameObjects(ctx);
+  //   drawGameObjects(ctx);
+
+  eventEmitter.on(Messages.COLLISION_ENEMY_MAIN, () => {
+    mainCharacter.color = "blue";
+  });
+
+  eventEmitter.on(Messages.KEY_EVENT_LEFT, () => {
+    mainCharacter.x -= 5;
+  });
+
+  eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
+    mainCharacter.x += 5;
+  });
 }
 
 function spawnEnemies() {
   timeSpawn = 1000;
-  let centro = canvas.width/2 -25;
+  let centro = canvas.width / 2 - 25;
   let separacion = 75;
   spawnPoints = [
-    centro -2*separacion,
-    centro -separacion,
-    centro ,
-    centro +separacion,
-    centro +2*separacion
-    
+    centro - 2 * separacion,
+    centro - separacion,
+    centro,
+    centro + separacion,
+    centro + 2 * separacion,
+
     // 0,
     // canvas.width/2 - 50,
-    // canvas.width/2 + canvas.width/4 - 50, 
-    ];
+    // canvas.width/2 + canvas.width/4 - 50,
+  ];
   let y = 0;
   let x = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
   let id = setInterval(() => {
@@ -96,7 +166,6 @@ function spawnEnemies() {
     x = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
     const enemy = new Enemy(x, y);
     gameObjects.push(enemy);
-    
   }, timeSpawn);
 }
 
@@ -112,21 +181,50 @@ function drawGameObjects(ctx) {
   gameObjects.forEach((go) => go.draw(ctx));
 }
 
-function updateGameObjects() {}
+function intersectRect(r1, r2) {
+  //funcion para comprobar si dos objetos estan colisionando
+  return !(
+    r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top
+  );
+}
+
+function updateGameObjects() {
+  //comprobar si hay colisiones
+  const enemies = gameObjects.filter((go) => go.type === "Enemy");
+  const mainCharacter = gameObjects.filter((go) => go.type === "MainCharacter");
+  mainCharacter.forEach((main) => {
+    enemies.forEach((enemy) => {
+      //detectar colisiones
+      if (
+        intersectRect(main.rectFromGameObject(), enemy.rectFromGameObject())
+      ) {
+        eventEmitter.emit(Messages.COLLISION_ENEMY_MAIN, {
+          first: main,
+          second: enemy,
+        });
+      }
+    });
+  });
+
+  //borrar enemigos muertos
+  gameObjects = gameObjects.filter((go) => !go.dead);
+}
 
 window.onload = () => {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  console.log(canvas, ctx);
-  (mainCharacterColor = "green"),
-    (mainCharacterImpactColor = "yellow"),
-    initGame();
+  
+  initGame();
   let gameLoopId = setInterval(() => {
     //borrar canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     //actualizar objetos del juego
+    updateGameObjects();
     //dibujar objetos del juego
     drawGameObjects(ctx);
   }, 100);
