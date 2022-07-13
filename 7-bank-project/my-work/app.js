aa678f2206f3bf32f3da0d59c074f2f5aa2d171a
saltimbanki <jorgebabarro@hotmail.com>
2022-07-13 15:56:07 +0200

    recuperacion de datos de login

--- 7-bank-project/my-work/app.js
+++ 7-bank-project/my-work/app.js
@@ -1,82 +1,121 @@
 const routes = {
-    '/login': { templateId: 'login', title: 'Login' },
-    '/dashboard': { templateId: 'dashboard' , title: 'Dashboard', method : writeToConsole},
-    '/credits': {templateId: 'credits', title: 'Credits'},
-  };
+  "/login": { templateId: "login", title: "Login" },
+  "/dashboard": {
+    templateId: "dashboard",
+    title: "Dashboard",
+    method: updateDashboard,
+  },
+  "/credits": { templateId: "credits", title: "Credits" },
+};
 
+let account = null;
 
-function updateRoute(templateId){
-    const path = window.location.pathname;
-    const route = routes[path];
+function updateRoute(templateId) {
+  const path = window.location.pathname;
+  const route = routes[path];
 
-    if(!route){
-        return navigate('/login');
-    }
+  if (!route) {
+    return navigate("/login");
+  }
+
+  const template = document.getElementById(route.templateId);
+
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById("app");
+  document.title = route.title;
+  if (typeof route.method === 'function') {
+    // debugger;
+    route.method();
     
-    
-    const template = document.getElementById(route.templateId);
-    
-    const view = template.content.cloneNode(true);
-    const app = document.getElementById('app');
-    document.title = route.title;
-    if(route.method){
-        route.method(route.title);
-    }
-    app.innerHTML = '';
-    app.appendChild(view);
+  }
+  app.innerHTML = "";
+  app.appendChild(view);
 }
 
-function navigate(path){
-    window.history.pushState({},path, path);
-    updateRoute();
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
 }
 
-function onLinkClick(event){
-    event.preventDefault();
-    navigate(event.target.href);
-    console.log(event.target.href);
+function onLinkClick(event) {
+  event.preventDefault();
+  navigate(event.target.href);
+  console.log(event.target.href);
 }
 
-function writeToConsole(title){
-    console.log(`${title} is shown`);
+function writeToConsole(title) {
+  console.log(`${title} is shown`);
 }
 
+async function register() {
+  const registerForm = document.getElementById("registerForm");
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
 
-
-
-async function register(){
-    
-    const registerForm = document.getElementById('registerForm');
-    const formData = new FormData(registerForm);
-    const data = Object.fromEntries(formData);
-    const jsonData = JSON.stringify(data);
-
-    const result = await createAccount(jsonData);
-    //controlar si todo esta ok
-    if(result.error){
-        return console.log('An error occurred: ', result.error);
-    }
-    console.log('Account created!', result);
+  const result = await createAccount(jsonData);
+  //controlar si todo esta ok
+  if (result.error) {
+    return console.log("An error occurred: ", result.error);
+  }
+  console.log("Account created!", result);
 }
 
 async function createAccount(account) {
-    try {
-      const response = await fetch('//localhost:5000/api/accounts', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: account
-      });
-      return await response.json();
-    } catch (error) {
-      return { error: error.message || 'Unknown error' };
-    }
+  try {
+    const response = await fetch("//localhost:5000/api/accounts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: account,
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || "Unknown error" };
+  }
+}
+async function getAccount(user) {
+  try {
+    const response = await fetch(
+      "//localhost:5000/api/accounts/" + encodeURIComponent(user)
+    );
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || "Unknown error" };
+  }
+}
+
+async function login() {
+  const loginForm = document.getElementById("loginForm");
+  const user = loginForm.user.value;
+  const data = await getAccount(user);
+  console.log(data);
+  if (data.error) {
+    return updateElement("loginError", data.error);
   }
 
-window.onload = ()=>{
-    updateRoute();
+  account = data;
+  navigate("/dashboard");
+}
+
+function updateElement(id, text) {
+  const element = document.getElementById(id);
+  element.textContent = text;
+}
+
+function updateDashboard() {
+  if (!account) {
+    return navigate("/login");
+  }
+  updateElement("description", account.description);
+  updateElement("balance", account.balance.toFixed(2));
+  updateElement("currency", account.currency);
+}
+
+window.onload = () => {
+  updateRoute();
 };
 
-window.onpopstate = ()=>{
-    //actualiza cuando usamos el historial del navegador para navegar
-    updateRoute();
-}
+window.onpopstate = () => {
+  //actualiza cuando usamos el historial del navegador para navegar
+  updateRoute();
+};
